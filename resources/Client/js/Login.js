2487c47db5f69259e8013271ace51cf6b530a37f
James <87453@farnborough.ac.uk>
2019-12-11 22:16:09 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/.gitignore
    #	.idea/misc.xml
    #	.idea/vcs.xml
    #	CourseWork.iml
    #	target/classes/Controllers/Event.class
    #	target/classes/Controllers/Genre.class
    #	target/classes/Controllers/Post.class
    #	target/classes/Controllers/SocialLink.class
    #	target/classes/Controllers/User.class

---
+++ resources/Client/js/Login.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+    document.getElementById("loginButton").addEventListener("click", login);}
+
+function login(){
+    const form = document.getElementById("loginForm");
+    const formData = new FormData(form);
+}
