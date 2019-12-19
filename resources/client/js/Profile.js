a3e9f5a9d187263f10aba1799ca6649816e9cec6
James <87453@farnborough.ac.uk>
2019-12-19 22:39:47 +0000

    fixed login and sign up - Constraints are broken on SignUp Functions
    Merge remote-tracking branch 'origin/master'
    
    # Conflicts:
    #	.idea/.gitignore
    #	.idea/misc.xml
    #	.idea/vcs.xml
    #	CourseWork.iml
    #	target/classes/Controllers/Event.class
    #	target/classes/Controllers/Genre.class
    #	target/classes/Controllers/Post.class
    #	target/classes/Controllers/SocialLink.class
    #	target/classes/Controllers/User.class

--- resources/client/js/Profile.js
+++ resources/client/js/Profile.js
@@ -1,1 +1,24 @@
 
+function pageLoad(){
+    let profileHTML = '<table>'
+        + '<tr>'
+    + '<th>Name</th>' + '<th>DateJoined</th>' + '<th>Followers</th>' + '<th>Following</th>'+ '<th>Bio</th>'+
+        '</tr>';
+    fetch('/user/display{id}', {method: 'get'}
+    ).then(response => response.json()
+    ).then(datas => {
+        for (let data of datas) {
+            profileHTML += `<tr>` +
+                `<td>${data.name}</td>` +
+                `<td>${data.DateJoined}</td>` +
+                `<td>${data.Followers}</td>` +
+                `<td>${data.Following}</td>` +
+                `<td>${data.Bio}</td>` +
+        }
+        profileHTML += '</table>';
+
+        document.getElementById("listDiv").innerHTML = profileHTML;
+    });
+
+
+    }
