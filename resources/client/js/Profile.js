8c1a49eb308f366698ceca4a6e707d4f3e18c197
James <87453@farnborough.ac.uk>
2019-12-21 15:31:10 +0000

    fixed login and sign up - Constraints are broken on SignUp Functions
    Merge remote-tracking branch 'origin/master'
    
    # Conflicts:
    #	.idea/.gitignore
    #	.idea/misc.xml
    #	.idea/vcs.xml
    #	CourseWork.iml
    #	target/classes/Controllers/Event.class
    #	target/classes/Controllers/Genre.class
    #	target/classes/Controllers/Post.class
    #	target/classes/Controllers/SocialLink.class
    #	target/classes/Controllers/User.class

--- resources/client/js/Profile.js
+++ resources/client/js/Profile.js
@@ -1,19 +1,20 @@
 
-function pageLoad(){
+function pageLoad(event){
+    const id = event.target.getAttribute("data-id");
     let profileHTML = '<table>'
         + '<tr>'
     + '<th>Name</th>' + '<th>DateJoined</th>' + '<th>Followers</th>' + '<th>Following</th>'+ '<th>Bio</th>'+
         '</tr>';
-    fetch('/user/display{id}', {method: 'get'}
+    fetch('/user/display' +id , {method: 'get'}
     ).then(response => response.json()
     ).then(datas => {
         for (let data of datas) {
             profileHTML += `<tr>` +
                 `<td>${data.name}</td>` +
-                `<td>${data.DateJoined}</td>` +
-                `<td>${data.Followers}</td>` +
-                `<td>${data.Following}</td>` +
-                `<td>${data.Bio}</td>` +
+                `<td>${data.name}</td>` +
+                `<td>${data.id}</td>` +
+                `<td>${data.id}</td>` +
+                `<td>${data.name}</td>`
         }
         profileHTML += '</table>';
 
