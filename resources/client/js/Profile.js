5646de92765c2a3204cf139aa1a788277db603c0
James <87453@farnborough.ac.uk>
2019-12-22 01:02:27 +0000

    fixed login and sign up - Constraints are broken on SignUp Functions
    :)
    
    Merge remote-tracking branch 'origin/master'
    
    # Conflicts:
    #	.idea/.gitignore
    #	.idea/misc.xml
    #	.idea/vcs.xml
    #	CourseWork.iml
    #	target/classes/Controllers/Event.class
    #	target/classes/Controllers/Genre.class
    #	target/classes/Controllers/Post.class
    #	target/classes/Controllers/SocialLink.class
    #	target/classes/Controllers/User.class

--- resources/client/js/Profile.js
+++ resources/client/js/Profile.js
@@ -1,27 +1,45 @@
 
 function pageLoad(){
-    //const id = JSON.stringify(JSON.parse(window.sessionStorage.user));
-    //alert(id);
-    let id = 1;
-    let profileHTML = '<table>'
-        + '<tr>'
-    + '<th>Name</th>' + '<th>DateJoined</th>' + '<th>Followers</th>' + '<th>Following</th>'+ '<th>Bio</th>'+
-        '</tr>';
+
+    //parsing the logged in userID into the new javascript function//
+    let id = JSON.parse(window.sessionStorage.user);
+    id = JSON.stringify(id.userID);
+    alert(id);
+    // Java script to extract data from the database and display it on the users profile page//
+    let profileHTML = '<div>';
     fetch('/user/display/' +id , {method: 'get'}
     ).then(response => response.json()
     ).then(users => {
         for (let user of users) {
-            profileHTML += `<tr>` +
-                `<td>${user.firstName}</td>` +
-                `<td>${user.DateJoined}</td>` +
-                `<td>${user.Followers}</td>` +
-                `<td>${user.Following}</td>` +
-                `<td>${user.Bio}</td>`
+            profileHTML +=
+                    '<b>Name:</b>' +
+                    ' '+
+                    `${user.firstName}` +
+                    ' '+
+                    `${user.lastName}` +
+                '<p>' +
+                    '<b>DateJoined:</b>' +
+                    ' '+
+                    `${user.DateJoined}` +
+                '</p>'+
+                '<p>' +
+                    '<b>Followers:</b>' +
+                    ' '+
+                    `${user.Followers}` +
+                '</p>'+
+                '<p>' +
+                    '<b>Following:</b>' +
+                    ' '+
+                    `${user.Following}` +
+                '</p>'+
+                    '<b>Bio:</b>' +
+                '<br>'+
+                `${user.Bio}`
         }
-        profileHTML += '</table>';
-
+        profileHTML += '</div>';
         document.getElementById("listDiv").innerHTML = profileHTML;
     });
+    //-------------------------------------------------------------------------------//
 
 
     }
