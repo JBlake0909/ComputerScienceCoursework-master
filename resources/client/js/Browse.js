28ac0d0b31cd15a95fee811c86f20bd841466cfb
James <87453@farnborough.ac.uk>
2019-12-29 19:46:24 +0000

    fixed login and sign up - Constraints are broken on SignUp Functions
    :) more progress
    
    Merge remote-tracking branch 'origin/master'
    
    # Conflicts:
    #	.idea/.gitignore
    #	.idea/misc.xml
    #	.idea/vcs.xml
    #	CourseWork.iml
    #	target/classes/Controllers/Event.class
    #	target/classes/Controllers/Genre.class
    #	target/classes/Controllers/Post.class
    #	target/classes/Controllers/SocialLink.class
    #	target/classes/Controllers/User.class

---
+++ resources/client/js/Browse.js
@@ -0,0 +1,4 @@
+window.onload=function(){
+    document.getElementById("GenSearchButton").addEventListener("click", login);
+    document.getElementById("UserSearchButton").addEventListener("click", signUp);
+};
